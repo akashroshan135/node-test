944a32ec7bf487d2f2713d395f8288ce7c52ee22
Akash Roshan <akash.second1999@gmail.com>
2020-10-28 12:04:07 +0530

    test console count

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("Hello World!!!");
+
+const x = 1;
+const y = 2;
+console.count(`the value of x is ${x}. printed times:`);
+console.count(`the value of x is ${x}. printed times:`);
+console.count(`the value of x is ${x}. printed times:`);
+console.count(`the value of y is ${y}. printed times:`);
+
+const oranges = ["orange", "orange", "orange"];
+const apples = ["apple"];
+oranges.forEach((fruit) => {
+	console.count(fruit);
+});
+apples.forEach((fruit) => {
+	console.count(fruit);
+});
