21106df9ff6096a4efc7072baa87574c84e04adb
Akash Roshan <akash.second1999@gmail.com>
2020-10-28 12:10:08 +0530

    test console time and trace

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-console.log("Hello World!!!");
+const test = () => console.trace("Hello World!!!");
 
-const x = 1;
-const y = 2;
-console.count(`the value of x is ${x}. printed times:`);
-console.count(`the value of x is ${x}. printed times:`);
-console.count(`the value of x is ${x}. printed times:`);
-console.count(`the value of y is ${y}. printed times:`);
-
-const oranges = ["orange", "orange", "orange"];
-const apples = ["apple"];
-oranges.forEach((fruit) => {
-	console.count(fruit);
-});
-apples.forEach((fruit) => {
-	console.count(fruit);
-});
+const calcTime = () => {
+	console.time("time");
+	for (let i = 0; i < 10000000; i++) {}
+	test();
+	console.timeEnd("time");
+};
+calcTime();
