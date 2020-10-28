9cba3cdb46c92cb8f86855d592265404d48fd0c9
Akash Roshan <akash.second1999@gmail.com>
2020-10-28 12:42:12 +0530

    test path module

--- app.js
+++ app.js
@@ -1,7 +1,1 @@
-const Person = require("./person");
-
-// console.log(person);
-// console.log(`Hello ${person.name}!!!`);
-
-const p1 = new Person("Bob");
-p1.greeting();
+console.log("Hello World!!!");
