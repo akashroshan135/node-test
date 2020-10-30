3df92761d0ad37bfb7e97dd2ad676b32c1c5e1dd
Akash Roshan <akash.second1999@gmail.com>
2020-10-30 16:58:04 +0530

    edit small change

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
 	if (req.url === "/") {
-		fs.readFile(path.join(__dirname, "public", "index.html"), (err, content) => {
+		fs.readFile(path.resolve("public", "index.html"), (err, content) => {
 			if (err) throw err;
 			res.writeHead(200, { "Content-Type": "text/html" });
 			res.end(content);
 		});
 	}
 	if (req.url === "/about") {
-		fs.readFile(path.join(__dirname, "public", "about.html"), (err, content) => {
+		fs.readFile(path.resolve("public", "about.html"), (err, content) => {
 			if (err) throw err;
 			res.writeHead(200, { "Content-Type": "text/html" });
 			res.end(content);
