f47c9203e237cdcc4e387470827cd635da4a4e5b
Akash Roshan <akash.second1999@gmail.com>
2020-10-30 16:35:36 +0530

    render html through node http

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 const port = 3000;
 
-// create server
-http.createServer((req, res) => {
-	res.write("Hello World!!");
-	res.end();
-}).listen(port, () => console.log(`App listening at http://localhost:${port}`));
+const server = http.createServer((req, res) => {
+	if (req.url === "/") {
+		fs.readFile(path.join(__dirname, "public", "index.html"), (err, content) => {
+			if (err) throw err;
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.end(content);
+		});
+	}
+	if (req.url === "/about") {
+		fs.readFile(path.join(__dirname, "public", "about.html"), (err, content) => {
+			if (err) throw err;
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.end(content);
+		});
+	}
+});
+
+server.listen(port, () => console.log(`App listening at http://localhost:${port}`));
