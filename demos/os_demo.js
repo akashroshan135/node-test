7de06cb67aa6a5d2c4b4b92e8deb21f746536736
Akash Roshan <akash.second1999@gmail.com>
2020-10-30 15:13:53 +0530

    test os module

---
+++ demos/os_demo.js
@@ -0,0 +1,18 @@
+const os = require("os");
+
+// platform
+console.log(os.platform());
+
+// cpu architecture
+console.log(os.arch());
+
+// cpu core stuff
+console.log(os.cpus());
+
+// free memory
+console.log(os.freemem());
+
+// total memory
+console.log(os.totalmem());
+
+// this is kinda boring but i dont wanna leave anything that might be important or something later when i wanna do some cool shit with this
